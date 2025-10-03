7d426cef252a043087af1b71b36c6b584a379915
Кольцов Алексей <i@kaytmb.ru>
2025-10-03 12:33:56 +0300

    fc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -29,9 +29,12 @@ export default defineConfig({
     emptyOutDir: true,
   },
   server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8989',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
 });
