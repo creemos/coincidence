79b6aa97a830b0af5ee27cab030c654c54ea9036
Кольцов Алексей <i@kaytmb.ru>
2025-10-06 09:47:58 +0300

    fc

--- backend/src/main/java/ru/coincidence/config/DataLoader.java
+++ backend/src/main/java/ru/coincidence/config/DataLoader.java
@@ -1,16 +1,27 @@
 package ru.coincidence.config;
 
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.coincidence.entity.User;
+import ru.coincidence.repository.UserRepository;
+
+import java.util.Optional;
 
 @Configuration
 public class DataLoader {
 
-    /*@Bean
+    @Bean
     CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
-            userRepository.save(new User("admin", passwordEncoder.encode("123456"), User.Role.ADMIN));
+            Optional<User> admin = userRepository.findByLogin("admin");
+            User a = admin.get();
+            a.setPassword(passwordEncoder.encode("123456"));
+            userRepository.save(a);
+            //userRepository.save(new User("admin", passwordEncoder.encode("123456"), User.Role.ADMIN));
         };
-    }*/
+    }
 
 }
